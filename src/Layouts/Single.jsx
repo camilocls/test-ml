129124a045807b675b17f55dcd4645719dd50c65
camilocls <camiloclsx@gmail.com>
2021-06-14 23:50:19 -0500

    Add single layout

---
+++ src/Layouts/Single.jsx
@@ -0,0 +1,15 @@
+import Header from "../Components/Header";
+import Wrapper from "../Components/Wrapper";
+
+function SingleLayout({ children }) {
+  return (
+    <div className="page-layout">
+      <Header />
+      <Wrapper>
+        { children }
+      </Wrapper>
+    </div>
+  );
+}
+
+export default SingleLayout;
