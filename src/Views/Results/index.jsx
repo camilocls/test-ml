0c5982cba205f8201121b45363457e53af08abca
camilocls <camiloclsx@gmail.com>
2021-06-15 01:25:19 -0500

    Add product list view

--- src/Views/Results/index.jsx
+++ src/Views/Results/index.jsx
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 import { getItemsBySearch } from "../../services/getItemsBySearch";
-import Error from "../../Components/Error"
-import Loader from "../../Components/Loader"
+import Error from "../../Components/Error";
+import Loader from "../../Components/Loader";
+import EmptyState from "../../Components/EmptyState";
+import ProductList from "../../Components/ProductList";
+import BreadcrumbLayout from "../../Layouts/Breadcrumb";
 
 function Results(props) {
   let { search } = useLocation();
   const [breadcrumb, setBreadcrumb] = useState("");
   const [isLoading, setIsLoading] = useState(true);
+  const [results, setResults] = useState([]);
   const [isError, setIsError] = useState(false);
   const values = queryString.parse(search);
   const { q } = values;
@@ -16,14 +20,19 @@ function Results(props) {
   useEffect(() => {
     setIsLoading(true);
     setIsError(false);
+    setResults([]);
 
     getItemsBySearch(q).then((data) => {
       if (data.error) {
         setIsError(true);
-      } else {
-        setIsLoading(false);
-        setBreadcrumb(data.breadcrumb);
       }
+
+      if (data && data.items && data.items.length) {
+        setBreadcrumb(data.breadcrumb);
+        setResults(data.items);
+      }
+
+      setIsLoading(false);
     });
   }, [q]);
 
@@ -35,11 +44,16 @@ function Results(props) {
     return <Loader />;
   }
 
+  if (!results.length) {
+    return <EmptyState query={q} />;
+  }
+
   return (
-    <div className="results">
-      {breadcrumb}
-      Results
-    </div>
+    <BreadcrumbLayout breadcrumb={breadcrumb}>
+      <div className="results">
+        <ProductList items={results} />
+      </div>
+    </BreadcrumbLayout>
   );
 }
 
