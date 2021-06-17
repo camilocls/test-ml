2c2628d752b4f12694e3e8ab86489afc42e72634
camilocls <camiloclsx@gmail.com>
2021-06-17 00:58:03 -0500

    Initial view product

--- src/Views/Product/index.jsx
+++ src/Views/Product/index.jsx
@@ -1,7 +1,27 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import Error from "../../Components/Error";
+import Loader from "../../Components/Loader";
+import { useDataItem } from "../../Hooks/useDataItem";
+
 function Product() {
+  const { id: productId } = useParams();
+  const { isError, isFetching, title, price, picture, description } =
+    useDataItem(productId);
+
+  if (isError) {
+    return <Error />;
+  }
+
+  if (isFetching) {
+    return <Loader />;
+  }
+
   return (
     <div className="product">
-      This is my Product page MELI!
+      <h1>{title}</h1>
+      <img src={picture} alt={title} />
+      <p>{description}</p>
     </div>
   );
 }
