a2e1716fa822cf092a6592b87661abd6809deeba
camilocls <camiloclsx@gmail.com>
2021-06-17 00:57:23 -0500

    Use Custom hook data

--- src/Views/Results/index.jsx
+++ src/Views/Results/index.jsx
@@ -1,57 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
-import { getItemsBySearch } from "../../services/getItemsBySearch";
+import { useDataSearch } from "../../Hooks/useDataSearch";
 import Error from "../../Components/Error";
 import Loader from "../../Components/Loader";
 import EmptyState from "../../Components/EmptyState";
 import ProductList from "../../Components/ProductList";
 import BreadcrumbLayout from "../../Layouts/Breadcrumb";
 
-function Results(props) {
-  let { search } = useLocation();
-  const [breadcrumb, setBreadcrumb] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
-  const [results, setResults] = useState([]);
-  const [isError, setIsError] = useState(false);
+function Results() {
+  const { search } = useLocation();
   const values = queryString.parse(search);
-  const { q } = values;
-
-  useEffect(() => {
-    setIsLoading(true);
-    setIsError(false);
-    setResults([]);
-
-    getItemsBySearch(q).then((data) => {
-      if (data.error) {
-        setIsError(true);
-      }
-
-      if (data && data.items && data.items.length) {
-        setBreadcrumb(data.breadcrumb);
-        setResults(data.items);
-      }
-
-      setIsLoading(false);
-    });
-  }, [q]);
+  const { q: query } = values;
+  const { isError, isFetching, breadcrumb, items } = useDataSearch(query);
 
   if (isError) {
     return <Error />;
   }
 
-  if (isLoading) {
+  if (isFetching) {
     return <Loader />;
   }
 
-  if (!results.length) {
-    return <EmptyState query={q} />;
+  if (items && !items.length) {
+    return <EmptyState query={query} />;
   }
 
   return (
     <BreadcrumbLayout breadcrumb={breadcrumb}>
       <div className="results">
-        <ProductList items={results} />
+        <ProductList items={items} />
       </div>
     </BreadcrumbLayout>
   );
