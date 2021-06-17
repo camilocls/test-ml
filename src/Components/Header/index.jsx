b5dd6637f3cec911e05dad9e19adf3fe53552a79
camilocls <camiloclsx@gmail.com>
2021-06-17 13:43:10 -0500

    Remove Bootstrap grid

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,4 +1,3 @@
-import { Container, Row, Col } from "../../Components/Grid";
 import logo from "../../assets/logo.png";
 import Search from "../Search";
 import Wrapper from "../../Components/Wrapper";
@@ -8,20 +7,12 @@ function Header() {
   return (
     <div className="header">
       <Wrapper className="header__content">
-        <Container fluid>
-          <Row>
-            <Col md={{ span: 1, offset: 1 }}>
-              <a href="/">
-                <img className="header__logo" src={logo} alt="Logo MELI" />
-              </a>
-            </Col>
-            <Col md={9}>
-              <div className="header__search">
-                <Search />
-              </div>
-            </Col>
-          </Row>
-        </Container>
+        <a className="header__link-logo" href="/">
+          <img className="header__logo" src={logo} alt="Logo MELI" />
+        </a>
+        <div className="header__search">
+          <Search />
+        </div>
       </Wrapper>
     </div>
   );
--- src/Layouts/Single.jsx
+++ src/Layouts/Single.jsx
@@ -1,17 +1,12 @@
 import Header from "../Components/Header";
 import Wrapper from "../Components/Wrapper";
-import { Container, Row, Col } from "../Components/Grid";
 
 function SingleLayout({ children }) {
   return (
     <div className="page-layout">
       <Header />
       <Wrapper>
-        <Container fluid>
-          <Row>
-            <Col md={{ span: 10, offset: 1 }}>{children}</Col>
-          </Row>
-        </Container>
+        {children}
       </Wrapper>
     </div>
   );
