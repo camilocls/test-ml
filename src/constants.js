f3ccb528c19c7fcd68d797ef26bee9ca9a03333f
camilocls <camiloclsx@gmail.com>
2021-06-17 02:01:42 -0500

    Add const product conditions labels

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,6 @@
-export const API_URL = 'http://127.0.0.1:3001/api/';
+export const API_URL = "http://127.0.0.1:3001/api/";
+
+export const PRODUCT_CONDITIONS = {
+  new: "Nuevo",
+  used: "Usado",
+};
